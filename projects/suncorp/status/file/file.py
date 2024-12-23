77ddbe0a6917b899142cd479de4f45463cb80ac7
siddharthareddy535 <siddharthareddy535@gmail.com>
2024-12-23 22:54:26 +0530

    Improvements

--- projects/suncorp/status/file/file.py
+++ projects/suncorp/status/file/file.py
@@ -29,11 +29,11 @@ def mcid_func(retainVars = retainVars):
     row['p_key'] = retainVars['p_key']
     row['pgnb'] = retainVars['pgnb']
     res = []
-    row['str1'] = ''
+    row['str1'] = ''smKMAFKLMQDA;LVMKLFMSKLBVKDSAMV
     row['str2'] = ''
     row['str3'] = ''
     row['str4'] = ''
-    row['iostr5'] = ''smKMAFKLMQDA;LVMKLFMSKLBVKDSAMV
+    row['iostr5'] = ''
     row['str6'] = ''
     while retainvars['p_rtc'] != '1':
         callmodule('SAMCIDI', retainvars['pgnb'], retainvars['p_mod'], retainvars['p_gen'], retainvars['p_len'], retainvars['p_key'], retainvars['p_rtc'], row['str1'], row['str2'], row['str3'], row['iostr5'], row['str6'])
