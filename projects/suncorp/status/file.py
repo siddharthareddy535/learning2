a511376e657f9fa6fa96a6c673b03e75c3ec1754
siddharthareddy535 <siddharthareddy535@gmail.com>
2025-01-02 13:02:05 +0530

    v9_Enhancements_2025-01-02 13:02:04.973995

--- projects/suncorp/status/file.py
+++ projects/suncorp/status/file.py
@@ -21,7 +21,7 @@ retainVars['p_rtc'] = '9'
 retainVars['p_gen'] = 'gs'
 retainVars['p_key'] = ''
 retainVars['pgnb'] = _pgmno
-def updated():
+def fixed():
     pass
 def mcid_func(retainVars = retainVars):
     row['p_len'] = retainVars['p_len']
