ef2c7dbdee177f27a4f56a40d9dd3ffb1753cefd
siddharthareddy535 <siddharthareddy535@gmail.com>
2025-02-19 16:10:20 +0530

    v3_Enhancements_2025-02-19

--- projects/ing_poc/status/file1.py
+++ projects/ing_poc/status/file1.py
@@ -1,46 +1,38 @@
-#sasFilePath: fwrite_test.sas
-#conversionTime: 11/13/2024 10:43:29
-#linesInFile: 27 #linesOfCode: 19 #linesOfPython: 27
-#complexity: 1 #processedBlocks: 1 #passedBlocks: 1
-#failedBlocks: 0 #totalErrors: 0
-
-
-#%%
-scriptName='fwrite_test'
 import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from utils import *
 
-#%%
-START_BLOCK('Passed DataStep', '1:4384623882865929002 - Lines: 21 - 6 to 27 : 96% ')
-def updated():
-    pass
-result = getdf('result')
-def _null__func(df, row=None):
-    putBuffer=''
-    #FILE tpdet
-    f = open(tpdet, "a")
-    f.write(put([
-    'o=1', row['def_id'],
-    'o=251',
-    'CUSTODY INSTRUCTIONS',
-    'o=276', row['revaccid'],
-    'o=281', row['egbsetid'],
-    'o=311', row['sllid'],
-    'o=316', row['rsatypcd'],
-    'o=317', row['srvlvid'],
-    'o=318', row['trsid'],
-    'o=325', row['isisecid'],
-    'o=337', row['cmosecid'],
-    'o=362', begdt_formatudf(row['sdate.']),
-    'o=372', row['cevtypid'],
-    'o=375', ib4_formatudf(row['cevct']),
-    'o=379',
-    'CUSTODY INSTRUCTION TYPE',
-    'o=403', row['oper']]) + "\n")
-    f.close()
-    return row
-result_tmp = getdf('result')
-nulldf = result_tmp.apply(lambda row: _null__func(None, row ), axis=1)
-END_BLOCK('Passed DataStep', '1:4384623882865929002')
+def _null_func(df, file_path):
+    res = []
+
+    with open(file_path, 'w') as f:
+        for index, row in df.iterrows():
+            f.write(
+                row['def_id']
+                + 'CUSTODY INSTRUCTIONS'
+                + row['revaccid']
+                + row['egbsetid']
+                + row['sllid']
+                + row['rsatypcd']
+                + row['srvlvid']
+                + row['trsid']
+                + row['isisecid']
+                + row['cmosecid']
+                + row['sdate']
+                + row['begdt']
+                + row['cevtypid']
+                + row['cevct'] + ' ib4.'
+                + 'CUSTODY INSTRUCTION TYPE'
+                + row['oper']
+                + "\n"
+            )
+            res.append(row)
+
+    return res
+
+
+# Assuming 'result' is your DataFrame
+file_path='tpdet'
+tpdet = _null__func(result,file_path)
+tpdet_df = pd.DataFrame(tpdet)
--- projects/ing_poc/status/file2.py
+++ projects/ing_poc/status/file2.py
@@ -1,60 +1,57 @@
-#sasFilePath: if_else.sas
-#conversionTime: 11/13/2024 05:36:21
-#linesInFile: 52 #linesOfCode: 28 #linesOfPython: 33
-#complexity: 2 #processedBlocks: 4 #passedBlocks: 4
-#failedBlocks: 0 #totalErrors: 0
-
-
-#%%
-scriptName='if_else'
 import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import sys 
 from utils import *
 
-#%%
-START_BLOCK('Passed DataStep', '1:4662118624581971203 - Lines: 22 - 6 to 28 : 52% ')
-ctmspmr = getdf('ctmspmr')
-def updated_file_2():
-    pass
-ctmspmr['a'] = 1
-mapDFs['ctmspmr'] = ctmspmr
-def grd_func(df, row=None):
-    res = []
-    if (upcaseudf(row['lvl']) == 'PMR'):
-        if verify(row['inrid'], '-') == 0:
-            row['output_table'] = 'grd'
-            res.append(row.to_dict().copy())
-        else:
-            row['output_table'] = 'inr'
-            res.append(row.to_dict().copy())
-    elif (upcaseudf(row['lvl']) == 'ATY'):
-        row['output_table'] = 'aty'
-        res.append(row.to_dict().copy())
+
+keep_columns1=['lfcobjid' ,'tbl' ,'subkey' ,'excmdecd']
+keep_columns2=['lfcobjid','inrid' ,'tbl' ,'subkey' ,'excmdecd']
+keep_columns3=['lfcobjid' ,'inrid','tbl' ,'subkey' ,'excmdecd']
+
+def grid_func(df):
+    res=[]
+    for index, row in df.iterrows():
+        lvl = row['lvl'].upper()
+
+        if lvl == 'PMR':
+            if '-' not in str(row['inrid']):
+                row['output_table']='grd'
+            else:
+                row['output_table']='inr'
+        elif lvl == 'ATY':
+            row['output_table']='aty'
+        res.append(row)
+    
     return res
-ctmspmr_tmp = getdf('ctmspmr')
-grd = ctmspmr_tmp.apply(lambda row: grd_func(None, row ), axis=1)
-grd_base  = pandas.createPandasDFFromDicts(grd)
-grd = cxt.getOutputDF(grd_base, 'grd')
-inr = cxt.getOutputDF(grd_base, 'inr')
-aty = cxt.getOutputDF(grd_base, 'aty')
-grd = keepColumns(grd, ['lfcobjid','tbl','subkey','excmdecd'])
-inr = keepColumns(inr, ['lfcobjid','inrid','tbl','subkey','excmdecd'])
-aty = keepColumns(aty, ['lfcobjid','inrid','tbl','subkey','excmdecd'])
-END_BLOCK('Passed DataStep', '1:4662118624581971203')
 
-#%%
-START_BLOCK('Passed IFBLOCK', '2:4662118624581971203 - Lines: 10 - 29 to 39 : 73% ')
-if (syserr or
- sysinfo
-):
-    {sysrput()}, rem_msg
-    {sysrput()}, rem_rc
-END_BLOCK('Passed IFBLOCK', '2:4662118624581971203')
+ctmspmr= pd.read_csv('ctmspmr.csv')
+data = grid_func(ctmspmr)
+df=pd.DataFrame(data)
+grd=df[df['output_table'] == 'grd']
+inr= df[df['output_table'] == 'inr']
+aty= df[df['output_table'] == 'aty']
+grd=grd[keep_columns1]
+inr=inr[keep_columns2]
+aty=aty[keep_columns3]
 
-#%%
-if (syserr or
- sysinfo
-):
-    '{sysrput()}, rem_msg == '', error, downloading, ctmsinr.'
-    '{sysrput()}, rem_rc'
+
+
+if syserr or sysinfo:
+    print(">>> Error downloading ctmspmr.")
+    REM_MSG = "Error downloading CTMSPMR."
+    REM_RC = 3
+    sys.stdout.write(f"REM_MSG: {REM_MSG}\n")
+    sys.stdout.write(f"REM_RC: {REM_RC}\n")
+
+
+if syserr or sysinfo:
+    print(">>> Error downloading ctmsinr.")
+    REM_MSG = "Error downloading CTMSINR."
+    REM_RC = 4
+    sys.stdout.write(f"REM_MSG: {REM_MSG}\n")
+    sys.stdout.write(f"REM_RC: {REM_RC}\n")
+
+
+
+
