3b30cf84f6c0fe7d7eb6f85c1f46d6ac7a3c5595
siddharthareddy535 <siddharthareddy535@gmail.com>
2025-02-19 15:53:40 +0530

    v2_Enhancements_2025-02-19

--- projects/infor/informatica/status/simple_expression_snowflake_informatica.py
+++ projects/infor/informatica/status/simple_expression_snowflake_informatica.py
@@ -1,34 +1,22 @@
-#sasFilePath: automation_sample/simple_expression.sas
-#conversionTime: 01/02/2025 10:41:16
-#linesInFile: 28 #linesOfCode: 18 #linesOfPython: 14
-#complexity: 1 #processedBlocks: 2 #passedBlocks: 2
-#failedBlocks: 0 #totalErrors: 0
-
 
 selectCols = [
-  (tbl("iods_den_imsmidas6"), col("Month_ID")),
-(tbl("iods_den_imsmidas6"), col("PackName_ID")),
-(tbl("iods_den_imsmidas6"), col("SALESKG_AMT"))
+    (tbl('iods_den_imsmidas6'), col('Month_ID')),
+    (tbl('iods_den_imsmidas6'), col('PackName_ID')),
+    (tbl('iods_den_imsmidas6'), col('tc_strength_AMT')),
+    (tbl('iods_den_imsmidas6'), col('su')),
+    (tbl('iods_den_imsmidas6'), col('saleskg_amt')),
 ]
 
 
-expression = [
-  """(
-                    CASE
-                        WHEN SALESKG_AMT IS NULL THEN SU*tc_strength_AMT
-                        ELSE SALESKG_AMT*1000000
-                    END
-                ) AS salesinmg_nbr"""
-]
-
-
+expression = ["""  CASE WHEN SALESKG_AMT IS NULL 
+    THEN SU * tc_strength_AMT
+    ELSE SALESKG_AMT * 1000000 END AS SALESINMG_NBR
+  """]
 
 informatica_insertinto(
-    source = (tbl("iods.iods_den_imsmidas6"), alias("iods_den_imsmidas6")),
-    target = tbl("work.etp1"),
-    columns = selectCols,
-expression = expression,
-truncate = False,
-distinct = False,
-
+    source = (tbl("iods.iods_den_imsmidas6"),alias("iods_den_imsmidas6")),
+    target=tbl("work.ETP1"),
+    columns=selectCols,
+    expression = expression,
+    distinct = False
 )
