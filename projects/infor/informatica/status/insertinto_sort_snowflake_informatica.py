69706dd354581d7ed6f11bd2cd918fa1157927e2
siddharthareddy535 <siddharthareddy535@gmail.com>
2025-02-19 15:59:14 +0530

    v3_Enhancements_2025-02-19

--- projects/infor/informatica/status/insertinto_sort_snowflake_informatica.py
+++ projects/infor/informatica/status/insertinto_sort_snowflake_informatica.py
@@ -1,6 +1,6 @@
-#sasFilePath: automation_sample/insertinto_sort.sas
-#conversionTime: 01/03/2025 08:16:45
-#linesInFile: 27 #linesOfCode: 17 #linesOfPython: 14
+#sasFilePath: automation_sample/sort.sas
+#conversionTime: 12/27/2024 12:15:02
+#linesInFile: 57 #linesOfCode: 47 #linesOfPython: 44
 #complexity: 1 #processedBlocks: 1 #passedBlocks: 1
 #failedBlocks: 0 #totalErrors: 0
 
@@ -10,13 +10,12 @@ selectCols = [
 (tbl("REP_EDW_FCT_IMSNATIONALSALES_01"), col("UCBBRAND_SK"))
 ]
 
-
+sortCols = ['countrycode_id','productclass_id','countrymonthindex_nbr','channelmonthindex_nbr']
 
 informatica_insertinto(
     source = (tbl("saswork.rep_edw_fct_imsnationalsales_01"), alias("REP_EDW_FCT_IMSNATIONALSALES_01")),
     target = tbl("rep.edw_fct_imsnationalsales"),
-    columns = selectCols,
 truncate = False,
 distinct = False,
-
+sortcols = sortCols
 )
--- projects/infor/informatica/status/simple_append_snowflake_informatica.py
+++ projects/infor/informatica/status/simple_append_snowflake_informatica.py
@@ -1,12 +1,13 @@
-#sasFilePath: automation_sample/simple_append.sas
-#conversionTime: 01/02/2025 10:37:34
+#sasFilePath: automation_sample/simple_append copy.sas
+#conversionTime: 12/23/2024 11:59:02
 #linesInFile: 14 #linesOfCode: 5 #linesOfPython: 3
-#complexity: 1 #processedBlocks: 2 #passedBlocks: 2
+#complexity: 1 #processedBlocks: 1 #passedBlocks: 1
 #failedBlocks: 0 #totalErrors: 0
 
+truncate = False
 
-informatica_appendinto(
-  source = (tbl("TBD"), alias("TBD")),
-  target_table = tbl("TBD"),
-  truncate = False
+informatica_insertinto(
+    source = (tbl("work.w2ow2y4g"), alias("w2ow2y4g")),
+    target = tbl("iods.iods_den_imsmidas7_etp"),
+    truncate = truncate
 )
